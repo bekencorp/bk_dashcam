59b9d6bf4538b92569cccaa0a3b56a8d0b6cb4c3
itester <itester@aclsemi.com>
2025-03-21 15:08:15 +0800

    [CI] [51573]  <multimedia> <perf> Add camera compatiblity code

--- projects/scooter/main/lvgl/lvgl_app_ui.c
+++ projects/scooter/main/lvgl/lvgl_app_ui.c
@@ -13,20 +13,26 @@ extern uint8_t lvgl_video_switch;
 extern void lv_example_meter(void);
 extern void lv_example_meter_exit(void);
 
+static lv_vnd_config_t lv_vnd_config = {0};
 void lvgl_event_open_handle(media_mailbox_msg_t *msg)
 {
     os_printf("[%s] lvgl open start\r\n", __func__);
 
     lvgl_video_switch = 1;
 
-    lv_vnd_config_t lv_vnd_config = {0};
     lcd_open_t *lcd_open = (lcd_open_t *)msg->param;
 
 #ifdef CONFIG_LVGL_USE_PSRAM
+#if 0
 #define PSRAM_DRAW_BUFFER ((0x60000000UL) + 5 * 1024 * 1024)
     lv_vnd_config.draw_pixel_size = ppi_to_pixel_x(lcd_open->device_ppi) * ppi_to_pixel_y(lcd_open->device_ppi);
     lv_vnd_config.draw_buf_2_1 = (lv_color_t *)PSRAM_DRAW_BUFFER;
     lv_vnd_config.draw_buf_2_2 = (lv_color_t *)(PSRAM_DRAW_BUFFER + lv_vnd_config.draw_pixel_size * sizeof(lv_color_t));
+#else
+    lv_vnd_config.draw_pixel_size = ppi_to_pixel_x(lcd_open->device_ppi) * ppi_to_pixel_y(lcd_open->device_ppi);
+    lv_vnd_config.draw_buf_2_1 = (lv_color_t *)frame_buffer_display_malloc(lv_vnd_config.draw_pixel_size * sizeof(lv_color_t));
+    lv_vnd_config.draw_buf_2_2 = (lv_color_t *)frame_buffer_display_malloc(lv_vnd_config.draw_pixel_size * sizeof(lv_color_t));
+#endif
 #else
 #define PSRAM_FRAME_BUFFER ((0x60000000UL) + 5 * 1024 * 1024)
     lv_vnd_config.draw_pixel_size = ppi_to_pixel_x(lcd_open->device_ppi) * ppi_to_pixel_y(lcd_open->device_ppi) / 10;
@@ -66,6 +72,19 @@ void lvgl_event_close_handle(media_mailbox_msg_t *msg)
     lvgl_video_switch = 0;
 
     lv_vendor_deinit();
+#ifdef CONFIG_LVGL_USE_PSRAM
+    if (lv_vnd_config.draw_buf_2_1)
+    {
+        frame_buffer_display_free((frame_buffer_t *)lv_vnd_config.draw_buf_2_1);
+        lv_vnd_config.draw_buf_2_1 = NULL;
+    }
+    if (lv_vnd_config.draw_buf_2_2)
+    {
+        frame_buffer_display_free((frame_buffer_t *)lv_vnd_config.draw_buf_2_2);
+        lv_vnd_config.draw_buf_2_2 = NULL;
+    }
+#endif
+    os_memset(&lv_vnd_config, 0, sizeof(lv_vnd_config_t));
 }
 
 void lvgl_event_handle(media_mailbox_msg_t *msg)
