2fce60efaa83d0705a692579f953f7f93e9fc4a2
itester <itester@aclsemi.com>
2025-06-13 11:56:45 +0800

    [CI] [60777]  <bt> <feat>add controller project

---
+++ projects/controller/main/app_main.c
@@ -0,0 +1,32 @@
+#include "bk_private/bk_init.h"
+#include <components/system.h>
+#include <os/os.h>
+#include <components/shell_task.h>
+#include <components/ate.h>
+
+extern void user_app_main(void);
+extern void rtos_set_user_app_entry(beken_thread_function_t entry);
+extern int bk_cli_init(void);
+extern void bk_set_jtag_mode(uint32_t cpu_id, uint32_t group_id);
+extern void controller_demo_init(void);
+
+void user_app_main(void){
+#if CONFIG_BTDM_CONTROLLER_ONLY
+    if (!ate_is_enabled())
+    {
+        controller_demo_init();
+    }
+#endif
+}
+
+int main(void)
+{
+#if (CONFIG_SYS_CPU0)
+	rtos_set_user_app_entry((beken_thread_function_t)user_app_main);
+	// bk_set_printf_sync(true);
+	// shell_set_log_level(BK_LOG_WARN);
+#endif
+	bk_init();
+
+	return 0;
+}
---
+++ projects/controller/main/controller_demo.c
@@ -0,0 +1,54 @@
+#include <components/system.h>
+#include <os/os.h>
+#include <components/log.h>
+#include "components/bluetooth/bk_ble.h"
+#include <os/mem.h>
+#include <os/str.h>
+
+#define TAG  "cp_test"
+
+#define LOGI(...) BK_LOGI(TAG, ##__VA_ARGS__)
+#define LOGW(...) BK_LOGW(TAG, ##__VA_ARGS__)
+#define LOGE(...) BK_LOGE(TAG, ##__VA_ARGS__)
+#define LOGD(...) BK_LOGD(TAG, ##__VA_ARGS__)
+
+static ble_err_t ble_hci_to_host_evt_cb(uint8_t *buf, uint16_t len)
+{
+    LOGI("recv hci evt: type(0x%02X), len(%d)\r\n",buf[0], len);
+
+    char *log = NULL;
+    uint32 log_len = len * 3 + 1;
+
+    log = (char *)os_malloc(log_len);
+
+    if (log)
+    {
+        os_memset(log, 0, log_len);
+
+        for (int i = 0; i < len; ++i)
+        {
+            os_snprintf(log + os_strlen(log), log_len - os_strlen(log), "%02X ", buf[i]);
+        }
+
+        LOGI("%s \r\n", log);
+        os_free(log);
+    }
+    return 0;
+}
+
+static ble_err_t ble_hci_to_host_acl_cb(uint8_t *buf, uint16_t len)
+{
+    return 0;
+}
+
+void controller_demo_init(void)
+{
+    bk_ble_reg_hci_recv_callback(ble_hci_to_host_evt_cb, ble_hci_to_host_acl_cb);
+
+    rtos_delay_milliseconds(1000);
+
+    uint8_t hci_reset_cmd[] = {0x01, 0x03, 0x0c, 0x00};
+
+    LOGI("Send hci reset cmd\r\n");
+    bk_ble_hci_to_controller(hci_reset_cmd[0], &hci_reset_cmd[1], sizeof(hci_reset_cmd) - 1);
+}
